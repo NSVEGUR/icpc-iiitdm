2734548ecdf9e0963bdfae6068379086026d292d
nsvegur <cs20b1016@iiitdm.ac.in>
2023-12-17 13:44:59 +0530

    fix: commentend unwanted corrections in results

--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -6,10 +6,10 @@
 
 <Main>
 	<div class="mb-5 w-full border-b-2 border-b-accent bg-muted py-10">
-		<h1 class="text-center text-2xl font-black">Results</h1>
+		<h1 class="text-center text-2xl font-black">Final Results</h1>
 	</div>
 	<section>
-		<article>
+		<!-- <article>
 			<p class="text-lg">
 				This is the selected list of teams. The selection is based on the criteria already
 				announced. This is a provisional selection list. Two days window is kept to address any
@@ -21,7 +21,7 @@
 					>preeth@iiitdm.ac.in</a
 				> by 11th Dec, 2023 10pm.
 			</p>
-		</article>
+		</article> -->
 		<Table
 			headings={['Rank', 'Name', 'College', 'Score', 'Penalty', 'Codedrills ID', 'External ID']}
 			rows={results}
