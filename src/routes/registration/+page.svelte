ea4aae43a33b1ca80186d11b9dae509429937cfb
nsvegur <cs20b1016@iiitdm.ac.in>
2024-01-01 13:55:53 +0530

    update: payment page marquee

--- src/routes/registration/+page.svelte
+++ src/routes/registration/+page.svelte
@@ -2,12 +2,21 @@
 	import Main from '$layouts/Main.svelte';
 	import Heading from '$ui/Heading.svelte';
 	import SubHeading from '$ui/SubHeading.svelte';
+	import New from '$images/new.gif';
+	import { Marquee } from 'flowbite-svelte';
 </script>
 
 <Main>
 	<div class="mb-5 w-full border-b-2 border-b-accent bg-muted py-10">
 		<h1 class="text-center text-2xl font-black">Registration</h1>
 	</div>
+	<Marquee speed={1} class="py-16" hoverSpeed={0.2} shadow>
+		<p class="relative mx-20 text-center font-bold text-red-700">
+			Due to technical glitch in payment page, the payment link will be activated from 02-01-2024,
+			12:00 noon.
+			<img src={New} alt="gif" class="absolute -top-10 right-0 h-8" />
+		</p>
+	</Marquee>
 	<section class="mb-10 text-justify text-lg">
 		<div>
 			The ICPC 2023 Onsite contest for Chennai region (Hosted by IIITDM Kancheepuram), is scheduled
