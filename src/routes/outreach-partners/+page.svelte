daaf7da227b10ceecc919c45be6000ba5b98f2d6
AravindanIIITDM <aravindan@iiitdm.ac.in>
2024-12-07 00:10:30 +0530

    Updates

--- src/routes/outreach-partners/+page.svelte
+++ src/routes/outreach-partners/+page.svelte
@@ -5,6 +5,7 @@
 	import Table from '$ui/Table.svelte';
 	import cclogo from '$images/cclogo.webp';
 	import geekslogo from '$images/geekslogo.webp';
+	import Jetbrains from '$images/Jetbrains.webp';
 
 </script>
 <Main>
@@ -12,8 +13,14 @@
 		<h1 class="text-center text-2xl font-black">Outreach Partners</h1>
 	</div>
 <div style="text-align center">	
-	<img src={cclogo} alt="icpc logo" width="500" height="200" style="display: block;" />
-
-		<img src={geekslogo} alt="icpc logo" width="500" height="500"/>
+	<h1 class="text-center text-2xl font-black">
+		<img src={cclogo} alt="icpc logo" width="500" height="200" style="display:inline-block" />
+	</h1>
+	<h1 class="text-center text-2xl font-black">
+		<img src={geekslogo} alt="icpc logo" width="500" height="500" style="display:inline-block;"/>
+	</h1>
+	<h1 class="text-center text-2xl font-black">
+		<img src={Jetbrains} alt="icpc logo" width="500" height="200" style="display:inline-block;"/>
+	</h1>
 </div>
 </Main>
