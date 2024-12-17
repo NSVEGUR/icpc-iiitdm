d4f104b0e7169effc058520365f11919441b1119
AravindanIIITDM <aravindan@iiitdm.ac.in>
2024-12-17 15:25:05 +0530

    Sponsors

--- src/lib/components/nav/Footer.svelte
+++ src/lib/components/nav/Footer.svelte
@@ -2,6 +2,11 @@
 	import Link from '$ui/Link.svelte';
 	import TitledLogo from '$images/titled_logo.webp';
 	import ICPCLogo from '$images/icpc.webp';
+	import pathway from '$images/pathway.webp';
+	import NST from '$images/NST.webp';
+	import jb from '$images/jb.webp';
+	import cclogo from '$images/cclogo.webp';
+	import geekslogo from '$images/geekslogo.webp';
 	import type { Link as LinkType } from '$lib/types';
 	export let hyperlinks: LinkType[];
 </script>
@@ -14,12 +19,37 @@
 		<a href="/" class="">
 			<img src={TitledLogo} alt="institute logo" class="h-16" />
 		</a>
+		<div class="flex items-center justify-center gap-10">
+			<p class="-2xs:text-xs text-center text-sm font-medium font-extrabold">
+				Sponsors
+				<a href="/" class="">
+					<img src={jb} alt="institute logo" class="h-16" />
+				</a>
+				<a href="/" class="">
+					<img src={pathway} alt="institute logo" class="h-16" />
+				</a>
+				<a href="/" class="">
+					<img src={NST} alt="institute logo" class="h-16" />
+				</a>
+			</p>
+			<p class="-2xs:text-xs text-center text-sm font-medium font-extrabold">
+				Outreach Partners
+				<a href="/" class="">
+					<img src={cclogo} alt="institute logo" class="h-16" />
+				</a>
+				<a href="/" class="">
+					<img src={geekslogo} alt="institute logo" class="h-16" />
+				</a>
+				
+			</p>
+		</div>
 	</div>
 	<div class="my-4 flex flex-wrap items-center justify-center gap-4">
 		{#each hyperlinks as { title, href }}
 			<Link {href}>{title}</Link>
 		{/each}
 	</div>
+	
 	<p class="-2xs:text-xs text-center text-sm font-medium">
 		© 2023, International Collegiate Programming Contest, Chennai
 	</p>
